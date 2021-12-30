314ebd5f77e0a5fbf9a1c82d473e9129b22bbd3d
Lennox Stevenson <lennox.stevenson@mail.utoronto.ca>
2021-12-29 21:06:39 -0500

    feat: day 7 part 1 + 2

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	day4 "github.com/steve148/advent-of-code-2021/day4"
 	day5 "github.com/steve148/advent-of-code-2021/day5"
 	day6 "github.com/steve148/advent-of-code-2021/day6"
+	"github.com/steve148/advent-of-code-2021/day7"
 )
 
 func main() {
@@ -25,5 +26,9 @@ func main() {
 		day6.Part1()
 	} else if day == "6" && part == "2" {
 		day6.Part2()
+	} else if day == "7" && part == "1" {
+		day7.Part1()
+	} else if day == "7" && part == "2" {
+		day7.Part2()
 	}
 }
